adc45e19e0929fab8c3838cb0d7d4ec67cda9394
Kevin Lu <klujan340@gmail.com>
2023-06-10 17:37:24 -0400

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
-const Footer = () => {
-  return <div></div>;
-};
+import React from 'react'
+import './Footer.css'
 
-export default Footer;
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer-container'>
+        <p> &copy; 2023 ToDo | All Rights Reserved</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
