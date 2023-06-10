ddb5905cfa495b0375d712beef669222826753e0
Kevin Lu <klujan340@gmail.com>
2023-06-10 17:44:38 -0400

    Modify Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import './Footer.css'
 function Footer() {
   return (
     <div className='footer'>
-      <div className='footer-container'>
-        <p> &copy; 2023 ToDo | All Rights Reserved</p>
-      </div>
+        <div className='footer-container'>
+          <p> &copy; 2023 ToDo | All Rights Reserved</p>
+        </div>
     </div>
   )
 }
