741bad09feeff1938634684f3ba3204130f4b1f3
Klu26 <klujan340@gmail.com>
2023-06-26 23:39:32 -0400

    Modify footer and add header logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './Header.css'
+import './Menu.js'
 
 function Header () {
   return (
-    <div className='header'>
-      <div className='header-container'>
-        <h1 className='logo'>ToDo</h1>
-        <ul className='menu'>
-          <li>Home</li>
+    <header className="site-header">
+    <div className="wrapper site-header__wrapper">
+      <nav className="nav">
+        <button className="nav__toggle" aria-expanded="false" type="button">
+          Lists
+        </button>
+        <ul className="nav__wrapper">
+          <li className="nav__item"><a>PlaceholderA</a></li>
+          <li className="nav__item"><a>PlaceholderB</a></li>
+          <li className="nav__item"><a>PlaceholderC</a></li>
+          <li className="nav__item"><a>PlaceholderD</a></li>
+          <li className="nav__item"><a>PlaceholderE</a></li>
         </ul>
-      </div>
+      </nav>
+      <a className="ToDo">Todo</a>
+      <a className="button button--icon" aria-label="Add">
+        <span>Add</span>
+      </a>
     </div>
+  </header>
   )
 }
 
 export default Header
+
